1de27cd5ed878d8766b0f222999bba6f219183bb
1Cezzo <emiljohnzen@gmail.com>
2024-02-11 23:39:57 +0100

    added rotation to bullet based on the angle of the bullet

--- scenes/bullet.gd
+++ scenes/bullet.gd
@@ -3,8 +3,14 @@ extends Area2D
 var direction : Vector2
 var speed : int = 500
 
+var sprite_rotation_initialized : bool = false
+
 func _process(delta):
 	position += speed * direction * delta
+	if direction.x != 0 && !sprite_rotation_initialized :
+		$Sprite2D.flip_h = direction.x < 0
+		sprite_rotation_initialized = true
+	#TODO place this logic into _ready if possible?
 
 func _on_timer_timeout():
 	queue_free()
