59de3adfe24ba151c8d2f9cf376387364cf97172
1Cezzo <emiljohnzen@gmail.com>
2024-02-17 23:30:28 +0100

    added functionality for completed waves

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,8 @@
 extends Node
 
 var max_enemies : int
+var difficulty : float
+const DIFF_MULTIPLIER : float = 1.2
 var wave : int
 var lives : int
 
@@ -10,9 +12,14 @@ func _ready():
 	$GameOver/Button.pressed.connect(new_game)
 
 func new_game():
-	max_enemies = 10
+	lives = 3
 	wave = 1
-	lives = 5
+	difficulty = 10.0
+	$EnemySpawner/Timer.wait_time = 1.0
+	reset()
+
+func reset():
+	max_enemies = int(difficulty)
 	$Player.reset()
 	get_tree().call_group("enemies", "queue_free")
 	get_tree().call_group("bullets", "queue_free")
@@ -25,16 +32,39 @@ func new_game():
 	$RestartTimer.start()
 
 func _process(_delta):
-	pass
+	if is_wave_completed():
+		wave += 1
+		difficulty *= DIFF_MULTIPLIER
+		if $EnemySpawner/Timer.wait_time > 0.25:
+			$EnemySpawner/Timer.wait_time -= 0.05
+		get_tree().paused = true
+		$WaveOverTimer.start()
+		
 
 func _on_enemy_spawner_hit_p():
 	lives -= 1
 	print("Hit player")
 	$HUD/LivesLabel.text = "X " + str(lives)
+	get_tree().paused = true
 	if lives <= 0:
-		get_tree().paused = true
 		$GameOver/WavesSurvivedLabel.text = "WAVES SURVIED: " + str(wave - 1)
 		$GameOver.show()
+	else:
+		$WaveOverTimer.start()
 
 func _on_restart_timer_timeout():
 	get_tree().paused = false
+
+func _on_wave_over_timer_timeout():
+	reset()
+
+func is_wave_completed():
+	var all_dead = true
+	var enemies = get_tree().get_nodes_in_group("enemies")
+	if enemies.size() == max_enemies:
+		for e in enemies:
+			if e.alive:
+				all_dead = false
+		return all_dead
+	else:
+		return false
